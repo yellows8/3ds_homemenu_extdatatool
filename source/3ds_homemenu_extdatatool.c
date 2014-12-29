8a31309c947c29cba8fa1bd5fd4df50d37b2de01
yellows8 <yellows8@users.noreply.github.com>
2014-12-29 17:54:29 -0500

    Implemented menus.

--- source/3ds_homemenu_extdatatool.c
+++ source/3ds_homemenu_extdatatool.c
@@ -25,6 +25,23 @@ FS_archive extdata_archive_theme = {
 
 u32 extdata_initialized = 0;
 
+typedef int (*menuent_funcptr)(void);
+
+int menu_savedatadat2sd();
+int menu_sd2savedatadat();
+int menu_enablethemecache();
+int menu_themecache2sd();
+int menu_sd2themecache();
+
+char *mainmenu_entries[5] = {
+"Copy extdata SaveData.dat to sd",
+"Copy SaveData.dat from sd to extdata",
+"Enable theme-cache",
+"Copy theme cache from extdata to sd",
+"Copy theme cache from sd to extdata"};
+int mainmenu_totalentries = 5;
+menuent_funcptr mainmenu_entryhandlers[5] = {menu_savedatadat2sd, menu_sd2savedatadat, menu_enablethemecache, menu_themecache2sd, menu_sd2themecache};
+
 Result open_extdata()
 {
 	Result ret=0;
@@ -74,9 +91,127 @@ void close_extdata()
 	}
 }
 
+int draw_menu(char **menu_entries, int total_menuentries, int x, int y)
+{
+	int i;
+	int cursor = 0;
+	int update_menu = 1;
+	int entermenu = 0;
+
+	while(aptMainLoop())
+	{
+		gspWaitForVBlank();
+		hidScanInput();
+
+		u32 kDown = hidKeysDown();
+
+		if(kDown & KEY_A)
+		{
+			entermenu = 1;
+			break;
+		}
+		if(kDown & KEY_B)return -1;
+
+		if(kDown & KEY_UP)
+		{
+			update_menu = 1;
+			cursor--;
+			if(cursor<0)cursor = total_menuentries-1;
+		}
+
+		if(kDown & KEY_DOWN)
+		{
+			update_menu = 1;
+			cursor++;
+			if(cursor>=total_menuentries)cursor = 0;
+		}
+
+		if(update_menu)
+		{
+			for(i=0; i<total_menuentries; i++)
+			{
+				if(cursor!=i)printf("\x1b[%d;%dH   %s", y+i, x, menu_entries[i]);
+				if(cursor==i)printf("\x1b[%d;%dH-> %s", y+i, x, menu_entries[i]);
+			}
+
+			gfxFlushBuffers();
+			gfxSwapBuffers();
+		}
+	}
+
+	if(!entermenu)return -2;
+	return cursor;
+}
+
+int menu_savedatadat2sd()
+{
+	printf("savedatadat2sd N/A\n");
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+	svcSleepThread(2000000000LL);
+	return -1;
+}
+
+int menu_sd2savedatadat()
+{
+	printf("sd2savedatadat N/A\n");
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+	svcSleepThread(2000000000LL);
+	return -1;
+}
+
+int menu_enablethemecache()
+{	
+	printf("enablethemecache N/A\n");
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+	svcSleepThread(2000000000LL);
+	return -1;
+}
+
+int menu_themecache2sd()
+{
+	printf("themecache2sd N/A\n");
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+	svcSleepThread(2000000000LL);
+	return -1;
+}
+
+int menu_sd2themecache()
+{
+	printf("sd2themecache N/A\n");
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+	svcSleepThread(2000000000LL);
+	return -1;
+}
+
+int handle_menus()
+{
+	int ret;
+
+	while(aptMainLoop())
+	{
+		consoleClear();
+
+		ret = draw_menu(mainmenu_entries, mainmenu_totalentries, 0, 0);
+		consoleClear();
+
+		if(ret<0)return ret;
+
+		ret = mainmenu_entryhandlers[ret]();
+		if(ret==-2)return ret;
+	}
+
+	return -2;
+}
+
 int main()
 {
 	Result extdata_openretval = 0;
+	Result ret=0;
 
 	// Initialize services
 	gfxInit();
@@ -87,21 +222,30 @@ int main()
 
 	printf("Opening extdata archives...\n");
 	extdata_openretval = open_extdata();
-	if(extdata_openretval==0)printf("Finished opening extdata.\n");
-
-	// Main loop
-	while (aptMainLoop())
+	if(extdata_openretval==0)
 	{
-		gspWaitForVBlank();
-		hidScanInput();
+		printf("Finished opening extdata.\n");
 
-		u32 kDown = hidKeysDown();
-		if (kDown & KEY_START)
-			break; // break in order to return to hbmenu
+		consoleClear();
+		ret = (Result)handle_menus();
+	}
 
-		// Flush and swap framebuffers
-		gfxFlushBuffers();
-		gfxSwapBuffers();
+	if(ret>=0)
+	{
+		// Main loop
+		while (aptMainLoop())
+		{
+			gspWaitForVBlank();
+			hidScanInput();
+
+			u32 kDown = hidKeysDown();
+			if (kDown & KEY_START)
+				break; // break in order to return to hbmenu
+
+			// Flush and swap framebuffers
+			gfxFlushBuffers();
+			gfxSwapBuffers();
+		}
 	}
 
 	close_extdata();
