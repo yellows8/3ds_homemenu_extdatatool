466946d13111fb47a7eef71e53b13cd1175b916f
yellows8 <yellows8@users.noreply.github.com>
2014-12-29 19:01:41 -0500

    Updated archive handling code to use arrays instead. Added archive_getfilesize(), which menu_savedatadat2sd() now uses.

--- source/3ds_homemenu_extdatatool.c
+++ source/3ds_homemenu_extdatatool.c
@@ -1,28 +1,16 @@
 #include <string.h>
 #include <stdio.h>
+#include <sys/stat.h>
 #include <3ds.h>
 
-u32 extdata_homemenu_lowpathdata[3];
-u32 extdata_theme_lowpathdata[3];
+typedef enum {
+	HomeMenu_Extdata,
+	Theme_Extdata,
+	TotalExtdataArchives
+} ExtdataArchive;
 
-FS_archive extdata_archive_homemenu = {
-	.id = ARCH_EXTDATA,
-	.lowPath = {
-		.type = PATH_BINARY,
-		.size = 0xc,
-		.data = (u8*)extdata_homemenu_lowpathdata
-	}
-};
-
-FS_archive extdata_archive_theme = {
-	.id = ARCH_EXTDATA,
-	.lowPath = {
-		.type = PATH_BINARY,
-		.size = 0xc,
-		.data = (u8*)extdata_theme_lowpathdata
-	}
-};
-
+u32 extdata_archives_lowpathdata[TotalExtdataArchives][3];
+FS_archive extdata_archives[TotalExtdataArchives];
 u32 extdata_initialized = 0;
 
 typedef int (*menuent_funcptr)(void);
@@ -33,33 +21,41 @@ int menu_enablethemecache();
 int menu_themecache2sd();
 int menu_sd2themecache();
 
+int mainmenu_totalentries = 5;
 char *mainmenu_entries[5] = {
 "Copy extdata SaveData.dat to sd",
 "Copy SaveData.dat from sd to extdata",
 "Enable theme-cache",
 "Copy theme cache from extdata to sd",
 "Copy theme cache from sd to extdata"};
-int mainmenu_totalentries = 5;
 menuent_funcptr mainmenu_entryhandlers[5] = {menu_savedatadat2sd, menu_sd2savedatadat, menu_enablethemecache, menu_themecache2sd, menu_sd2themecache};
+
+char *sdpath_prefix = "/3ds_homemenu_extdatatool/";
 
 Result open_extdata()
 {
 	Result ret=0;
+	u32 pos;
 	u32 extdataID_homemenu, extdataID_theme;
 
 	extdataID_homemenu = 0x0000008f;//Hard-coded for USA for now.
 	extdataID_theme = 0x000002cd;
 
-	memset(extdata_homemenu_lowpathdata, 0, 0xc);
-	memset(extdata_theme_lowpathdata, 0, 0xc);
+	for(pos=0; pos<TotalExtdataArchives; pos++)
+	{
+		extdata_archives[pos].id = ARCH_EXTDATA;
+		extdata_archives[pos].lowPath.type = PATH_BINARY;
+		extdata_archives[pos].lowPath.size = 0xc;
+		extdata_archives[pos].lowPath.data = (u8*)extdata_archives_lowpathdata[pos];
 
-	extdata_homemenu_lowpathdata[0] = 1;//mediatype, 1=SD
-	extdata_homemenu_lowpathdata[1] = extdataID_homemenu;//extdataID-low
+		memset(extdata_archives_lowpathdata[pos], 0, 0xc);
+		extdata_archives_lowpathdata[pos][0] = 1;//mediatype, 1=SD
+	}
 
-	extdata_theme_lowpathdata[0] = 1;//mediatype, 1=SD
-	extdata_theme_lowpathdata[1] = extdataID_theme;//extdataID-low
+	extdata_archives_lowpathdata[HomeMenu_Extdata][1] = extdataID_homemenu;//extdataID-low
+	extdata_archives_lowpathdata[Theme_Extdata][1] = extdataID_theme;//extdataID-low
 
-	ret = FSUSER_OpenArchive(NULL, &extdata_archive_homemenu);
+	ret = FSUSER_OpenArchive(NULL, &extdata_archives[HomeMenu_Extdata]);
 	if(ret!=0)
 	{
 		printf("Failed to open homemenu extdata with extdataID=0x%08x, retval: 0x%08x\n", (unsigned int)extdataID_homemenu, (unsigned int)ret);
@@ -67,7 +63,7 @@ Result open_extdata()
 	}
 	extdata_initialized |= 0x1;
 
-	ret = FSUSER_OpenArchive(NULL, &extdata_archive_theme);
+	ret = FSUSER_OpenArchive(NULL, &extdata_archives[Theme_Extdata]);
 	if(ret!=0)
 	{
 		printf("Failed to open theme extdata with extdataID=0x%08x, retval: 0x%08x\n", (unsigned int)extdataID_theme, (unsigned int)ret);
@@ -80,15 +76,39 @@ Result open_extdata()
 
 void close_extdata()
 {
-	if(extdata_initialized & 0x1)
+	u32 pos;
+
+	for(pos=0; pos<TotalExtdataArchives; pos++)
 	{
-		FSUSER_CloseArchive(NULL, &extdata_archive_homemenu);
+		if(extdata_initialized & (1<<pos))FSUSER_CloseArchive(NULL, &extdata_archives[pos]);
+	}
+}
+
+Result archive_getfilesize(int archive, char *path, u32 *outsize)
+{
+	Result ret=0;
+	struct stat filestats;
+	u64 tmp64=0;
+	Handle filehandle=0;
+
+	if(archive==-1)
+	{
+		if(stat(path, &filestats)==-1)return -1;
+
+		*outsize = filestats.st_size;
+
+		return 0;
 	}
 
-	if(extdata_initialized & 0x2)
-	{
-		FSUSER_CloseArchive(NULL, &extdata_archive_theme);
-	}
+	ret = FSUSER_OpenFile(NULL, &filehandle, extdata_archives[archive], FS_makePath(PATH_CHAR, path), 1, 0);
+	if(ret!=0)return ret;
+
+	ret = FSFILE_GetSize(filehandle, &tmp64);
+	if(ret==0)*outsize = (u32)tmp64;
+
+	FSFILE_Close(filehandle);
+
+	return ret;
 }
 
 int draw_menu(char **menu_entries, int total_menuentries, int x, int y)
@@ -145,7 +165,12 @@ int draw_menu(char **menu_entries, int total_menuentries, int x, int y)
 
 int menu_savedatadat2sd()
 {
-	printf("savedatadat2sd N/A\n");
+	Result ret=0;
+	u32 size=0;
+
+	ret = archive_getfilesize(HomeMenu_Extdata, "/SaveData.dat", &size);
+	printf("ret=0x%08x size=0x%08x\n", (unsigned int)ret, (unsigned int)size);
+
 	gfxFlushBuffers();
 	gfxSwapBuffers();
 	svcSleepThread(2000000000LL);
@@ -211,7 +236,6 @@ int handle_menus()
 int main()
 {
 	Result extdata_openretval = 0;
-	Result ret=0;
 
 	// Initialize services
 	gfxInit();
@@ -227,11 +251,12 @@ int main()
 		printf("Finished opening extdata.\n");
 
 		consoleClear();
-		ret = (Result)handle_menus();
+		handle_menus();
 	}
 
-	if(ret>=0)
+	if(extdata_openretval<0)
 	{
+		printf("Press the START button to exit.\n");
 		// Main loop
 		while (aptMainLoop())
 		{
